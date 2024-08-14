c4f839ed1d234a1b7adfc57a25a45fedf4072134
Groot-2001 <shivasilmawala841@gmail.com>
2024-08-15 00:01:52 +0530

    Birthday Remainder Starter code added

---
+++ birthday_remainder/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  return (
+    <div className="main">
+      <div className="container">Hi</div>
+    </div>
+  );
+}
+
+export default App;
